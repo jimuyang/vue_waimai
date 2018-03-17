dc9731460cca1c9f79b822485186d9409c82d628
muyi-macpro <17621660286@163.com>
2018-03-18 00:52:33 +0800

    transition-try

--- src/components/cart-control/cart-control.vue
+++ src/components/cart-control/cart-control.vue
@@ -3,6 +3,14 @@
     <div class="remove icon-remove_circle_outline" @click="addCart"></div>
     <div class="count">1</div>
     <div class="add icon-add_circle" @click="removeCart"></div>
+
+    <div class="ball-container">
+      <transition name="drop">
+        <div class="ball" v-for="ball in balls" v-show="ball.show">
+          <div class="inner"></div>
+        </div>
+      </transition>
+    </div>
   </div>
 
 </template>
@@ -15,8 +23,13 @@
         type: Object
       }
     },
+    data() {
+      return {
+        balls: []
+      }
+    },
     created() {
-      console.log(this.food);
+      // console.log(this.food);
     },
     methods: {
       addCart() {
@@ -45,4 +58,17 @@
       line-height 24px
       vertical-align top
       padding-top 6px
+    /*.ball-container*/
+      /*.drop-transition*/
+        /*transition all 0.4*/
+      /*.ball*/
+        /*position: fixed*/
+        /*left: 32px*/
+        /*bottom: 22px*/
+        /*z-index 200*/
+        /*.inner*/
+          /*width: 16px*/
+          /*height: 16px*/
+          /*background: #rgb(0, 160, 220)*/
+          /*border-radius 50%*/
 </style>
