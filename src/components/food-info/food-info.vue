a11e531a8d9702578956f6dcab1884203fbcc131
muyi-macpro <17621660286@163.com>
2018-04-25 23:46:29 +0800

    商品详情页

--- src/components/food-info/food-info.vue
+++ src/components/food-info/food-info.vue
@@ -7,7 +7,7 @@
         <div class="image-header">
           <img :src="food.image"/>
           <div class="back" @click="hide">
-            <i class="icon-arrow_lift"/>
+            <i class="icon-arrow_lift"></i>
           </div>
         </div>
 
@@ -23,8 +23,8 @@
             <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
           </div>
 
-          <div class="cartcontrol-wrapper">
-            <cartcontrol @add="addFood" :food="food"/>
+          <div class="cart-control-wrapper">
+            <cart-control @add="addFood" :food="food"/>
           </div>
 
           <transition name="fade">
@@ -37,33 +37,33 @@
         <!--<split v-show="food.info"/>-->
 
         <!--<div class="info" v-show="food.info">-->
-          <!--<h1 class="title">商品信息</h1>-->
-          <!--<p class="text">{{food.info}}</p>-->
+        <!--<h1 class="title">商品信息</h1>-->
+        <!--<p class="text">{{food.info}}</p>-->
         <!--</div>-->
 
         <!--<split/>-->
 
         <!--<div class="rating">-->
-          <!--<h1 class="title">商品评价</h1>-->
-          <!--<ratingselect @select="selectRating" @toggle="toggleContent" :selectType="selectType"-->
-                        <!--:onlyContent="onlyContent" :desc="desc"-->
-                        <!--:ratings="food.ratings"></ratingselect>-->
-          <!--<div class="rating-wrapper">-->
-            <!--<ul v-show="food.ratings && food.ratings.length">-->
-              <!--<li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings"-->
-                  <!--class="rating-item border-1px">-->
-                <!--<div class="user">-->
-                  <!--<span class="name">{{rating.username}}</span>-->
-                  <!--<img class="avatar" width="12" height="12" :src="rating.avatar">-->
-                <!--</div>-->
-                <!--<div class="time">{{rating.rateTime | formatDate}}</div>-->
-                <!--<p class="text">-->
-                  <!--<span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}-->
-                <!--</p>-->
-              <!--</li>-->
-            <!--</ul>-->
-            <!--<div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>-->
-          <!--</div>-->
+        <!--<h1 class="title">商品评价</h1>-->
+        <!--<ratingselect @select="selectRating" @toggle="toggleContent" :selectType="selectType"-->
+        <!--:onlyContent="onlyContent" :desc="desc"-->
+        <!--:ratings="food.ratings"></ratingselect>-->
+        <!--<div class="rating-wrapper">-->
+        <!--<ul v-show="food.ratings && food.ratings.length">-->
+        <!--<li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings"-->
+        <!--class="rating-item border-1px">-->
+        <!--<div class="user">-->
+        <!--<span class="name">{{rating.username}}</span>-->
+        <!--<img class="avatar" width="12" height="12" :src="rating.avatar">-->
+        <!--</div>-->
+        <!--<div class="time">{{rating.rateTime | formatDate}}</div>-->
+        <!--<p class="text">-->
+        <!--<span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}-->
+        <!--</p>-->
+        <!--</li>-->
+        <!--</ul>-->
+        <!--<div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>-->
+        <!--</div>-->
         <!--</div>-->
 
       </div>
@@ -84,6 +84,9 @@
         type: Object
       }
     },
+    components: {
+      'cart-control': CartControll
+    },
     data() {
       return {
         showFlag: false
@@ -102,9 +105,25 @@
             this.scroll.refresh();
           }
         });
+      },
 
-      }
+      // 商品详情页的添加购物车组件事件被抛出到goods组件
+      addFood(target) {
+        this.$emit('add', target);
+      },
 
+      hide() {
+        this.showFlag = false;
+      },
+
+      //第一次加入购物车
+      addFirst(event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.$emit('add', event.target);
+        Vue.set(this.food, 'count', 1);
+      },
 
     }
 
@@ -130,7 +149,81 @@
       transform: translate3d(100%, 0, 0)
 
     .image-header
-      position: absolute
-      width: 100% 
+      position: relative
+      width: 100%
+      height: 0
+      padding-top: 100%
+      img
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+      .back
+        position: absolute
+        top: 10px
+        left: 0
+        .icon-arrow_left
+          display: block
+          padding: 10px
+          font-size: 20px
+          color: #fff
+
+    .content
+      position: relative
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 8px
+        font-size: 14px
+        font-weight: 700
+        color: rgb(7, 17, 27)
+      .detail
+        margin-bottom: 18px
+        line-height 10px
+        height: 10px
+        font-size 0
+        .sell-count, .rating
+          font-size 10px
+          color: rgb(147, 153, 159)
+        .sell-count
+          margin-right: 12px
+      .price
+        font-weight: 700
+        line-height: 24px
+        .now
+          margin-right: 8px
+          font-size: 14px
+          color: rgb(240, 20, 20)
+        .old
+          text-decoration: line-through
+          font-size: 10px
+          color: rgb(147, 153, 159)
+      .cart-control-wrapper
+        position: absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 10
+        height: 24px
+        line-height: 24px
+        padding: 0 12px
+        box-sizing: border-box
+        border-radius: 12px
+        font-size: 10px
+        color: #fff
+        background: rgb(0, 160, 220)
+        opacity: 1
+        &.fade-enter-active, &.fade-leave-active
+          transition: all 0.2s
+        &.fade-enter, &.fade-leave-active
+          opacity: 0
+          z-index: -1
+
+
+
 
 </style>
